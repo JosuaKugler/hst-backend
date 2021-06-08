3ec2d50257dc0b59bafb997580f1f852b61f8570
JosuaKugler <josua.kugler@gmail.com>
2021-06-09 00:26:47 +0200

    create admin site

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Watchparty, Registration
+
+admin.site.register(User)
+admin.site.register(Watchparty)
+admin.site.register(Registration)
