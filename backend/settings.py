87275d241f1100b245b1e6e783132c645811e50f
JosuaKugler <josua.kugler@gmail.com>
2021-06-16 10:53:16 +0200

    add buttons

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = True
+DEBUG = False
 #ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '88.130.54.85']
 ALLOWED_HOSTS = ['*']
 
